3232735562b7305eb5040e45f2cb6d263cc750a4
Agniya551 <10hact10i@mail.ru>
2026-03-16 15:15:54 +0400

    Добавляет JSDoc и тесты

--- js/functions.js
+++ js/functions.js
@@ -1,34 +1,24 @@
-/*
-I. Функция для проверки длины строки.
-  Если длина строки меньше или равна второму параметру "length", то вернуть "true", иначе "false".
-  Принимает аргументы:
-    1. Строка - "string".
-    2. Максимальная длина - "length".
-*/
+/**
+ * Функция для проверки длины строки.
+ * @param {string} string - Передаваемая строка.
+ * @param {number} length - Максимальная длина строки.
+ * @return {boolean} - Если длина строки меньше или равна параметру "length", вернуть "true", иначе "false".
+ */
 
-const checkLengthString = (string, length) => string.length <= length;
+export const checkLengthString = (string = '', length = -1) => string.length <= length;
 
-/*
-II. Функция для проверки на палиндромность.
-  Если последовательность символов одинакова, при чтении, и справа налево и слева направо, то вернуть true, иначе false.
-  Регистр не учитывается.
-  Принимает аргумент "строку" - "string".
-*/
+/**
+ * Функция для проверки на палиндромность - последовательность символов одинакова, при чтении, и справа налево и слева направо.
+ * Регистр не учитывается.
+ * @param {string | number} string - Передаваемая строка.
+ * @return {boolean}
+ */
 
-const checkPalindrome = (string) => {
-  const clean = string.replaceAll(' ', '').toLowerCase(); // Нормализуем строку
-  let reverse = '';
-  for (let i = 1; i <= clean.length; i += 1) {
-    reverse += clean.at(-i);
+export const checkPalindrome = (string) => {
+  let clean = string;
+  if (typeof string === 'string') {
+    clean = string.replaceAll(' ', '').toLowerCase();
   }
-  return reverse === clean;
-};
-
-// Альтернативный метод
-// Последовательно проверяем совпадение символов сначала и с конца. И если находим несовпадение, то завершаем цикл и возвращаем false
-
-const checkPalindromeAlt = (string) => {
-  const clean = string.replaceAll(' ', '').toLowerCase();
   for (let i = 0; i < Math.floor(clean.length / 2); i ++) {
     if (clean[i] !== clean[clean.length - 1 - i]) {
       return false;
@@ -37,39 +27,20 @@ const checkPalindromeAlt = (string) => {
   return true;
 };
 
-/*
-Альтернативный метод - третий вариант
-  .split('') - разбивает строку на массив символов.
-  .reverse() - переворачивает порядок элементов массива.
-  .join('') - объединяет элементы массива в строку.
-*/
+/**
+ * Функцию для извлечения из строки цифр от 0 до 9.
+ * Результат вернуть одним, целым, положительным числом. Если в строке нет цифр - вернуть NaN.
+ * @param {string | number} string - Передаваемая строка.
+ * @return {number}
+ */
 
-const checkPalindromeAlt2 = (string) => {
-  const clean = string.replaceAll(' ', '').toLowerCase();
-  return clean === clean.split('').reverse().join('');
-};
-
-/*
-III. Функцию для извлечения цифр из строки.
-  Все цифры вернуть одним, целым, положительным числом.
-    1. Если в строке нет цифр - вернуть NaN.
-    2. Если число отрицательное - вернуть положительное число.
-    3. Если число дробное - вернуть целое число.
-  Принимает аргумент "строку" - "string".
-*/
-
-const extractNumbers = (string) => {
+export const extractNumbers = (string) => {
+  const str = String(string);
   let number = '';
-  for (let i = 0; i < string.length; i++) {
-    if (!isNaN(parseInt(string[i], 10))) {
-      number += string[i].toString();
+  for (let i = 0; i < str.length; i++) {
+    if (!isNaN(parseInt(str[i], 10))) {
+      number += str[i].toString();
     }
   }
   return parseInt(number, 10);
 };
-
-// Альтернативный метод
-// Регулярное выражение /\D/g заменяет ВСЕ не цифры на пустую строку. /g - глобальный флаг
-
-const extractNumbersAlt = (string) =>
-  parseInt(string.replace(/\D/g, ''), 10);
